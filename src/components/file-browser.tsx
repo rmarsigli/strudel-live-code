26b6ac623a662c42b3b38ed3c3b38cf091edf76f
Rafhael Marsigli <rmarsigli@gmail.com>
2025-10-24 12:13:52 -0300

    style: change archive selected  text color

--- src/components/file-browser.tsx
+++ src/components/file-browser.tsx
@@ -69,7 +69,7 @@ export function FileBrowser() {
                 <div
                   key={file.name}
                   className={`group flex items-center justify-between rounded-md px-3 py-2 text-sm hover:bg-accent cursor-pointer transition-colors ${
-                    currentFile?.name === file.name ? 'bg-accent' : ''
+                    currentFile?.name === file.name ? 'bg-accent text-slate-900' : ''
                   }`}
                   onClick={() => handleFileClick(file)}
                 >
